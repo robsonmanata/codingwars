544a7cd0935ad6de9ce9f1c1e15d7661af3bc690
robsonmanata <70637849+robsonmanata@users.noreply.github.com>
2023-12-03 01:34:05 +0100

    piano

--- index.js
+++ index.js
@@ -1,44 +1,90 @@
 
-function shownum() {
- let x = document.getElementsByTagName("input").value 
- let z =[document.getElementsByTagName("input")[0].value,document.getElementsByTagName("input")[1].value,document.getElementsByTagName("input")[2].value]  
+const audio1 = new Audio("c3-95007.mp3");
+const audio2 = new Audio("c6-102822.mp3");
+const audio3 = new Audio("f6-102819.mp3");
+const audio4 = new Audio("fa-78409.mp3");
+const audio5 = new Audio("g6-82014.mp3");
+const audio6 = new Audio("sol-101774.mp3");
 
- let i;
- for (i = 0; i < z.length; i++){
-    alert(z[i])
+function show(x) {
+  if (document.getElementById){
+   document.getElementById(x).style.visibility = "visible";
+   
+ }
+}
+  
+// blink "off" state
+function hide(x) {
+ if (document.getElementById){
+   document.getElementById(x).style.visibility = "hidden";
  }
 }
 
-function formdetail(){
-    event.preventDefault();
-    var formitems = document.getElementById("myform");
-    var i;
-    var item =""
-    for(i = 0; i < formitems.length; i++){
-        item += formitems.elements[i].value + "<br>";
-    }
-   
-    document.getElementById("mzform").innerHTML=item; 
-}
 
-//Asychronus javascript
-function displaydata(item){
-    document.getElementById("mzform").innerHTML=item;
-}
+ document.getElementById ("key1").innerhtml
+document.getElementById("key1").addEventListener("click", function(){
 
-const url = 'https://timetable-lookup.p.rapidapi.com/TimeTable/BOS/LAX/20191217/';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'fafd32854dmshe40ade0a301bae0p1703adjsndc5798bc1531',
-		'X-RapidAPI-Host': 'timetable-lookup.p.rapidapi.com'
-	}
-};
-async function start(){
-    const response = await fetch(url, options);
-	const result = await response.text();
-	displaydata(result);
+   audio1.play(); })
 
-}
-start();
-	
+   document.getElementById("key2").addEventListener("click", function(){
+    audio2.play(); })
+
+    document.getElementById("key3").addEventListener("click", function(){
+        audio3.play(); })
+
+        document.getElementById("key4").addEventListener("click", function(){
+            audio4.play(); })
+
+            document.getElementById("key5").addEventListener("click", function(){
+                audio5.play(); })
+
+                document.getElementById("key6").addEventListener("click", function(){
+                    audio6.play(); })
+                
+                    window.addEventListener(
+                        "keydown",
+                        (event) => {
+                          if (event.defaultPrevented) {
+                            return; // Do nothing if the event was already processed
+                          }
+                      
+                          switch (event.key) {
+                            case "a":
+                              hide("key1")
+                              setTimeout("show(`key1`)",200)
+                              audio1.play()
+                              break;
+                            case "d":
+                              hide("key2")
+                              setTimeout("show(`key2`)",200)
+                              audio2.play()
+                              break;
+                            case "g":
+                              hide("key3")
+                              setTimeout("show(`key3`)",200)
+                              audio3.play()
+                              break;
+                            case "j":
+                              hide("key4")
+                              setTimeout("show(`key4`)",200)
+                              audio4.play()
+                              break;
+                            case "l":
+                              hide("key5")
+                              setTimeout("show(`key5`)",200)
+                              audio5.play()
+                              break;
+                            case "p":
+                              hide("key6")
+                              setTimeout("show(`key6`)",200)
+                              audio6.play()
+                              break;
+                            default:
+                              return; // Quit when this doesn't handle the key event.
+                          }
+                      
+                          // Cancel the default action to avoid it being handled twice
+                          event.preventDefault();
+                        },
+                        true,
+                      );
